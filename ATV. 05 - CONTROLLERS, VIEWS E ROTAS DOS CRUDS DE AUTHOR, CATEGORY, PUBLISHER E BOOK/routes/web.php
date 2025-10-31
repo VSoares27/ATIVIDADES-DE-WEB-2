d70f67ee79a6fb7ca9b939fbe2377d7a22234f98
Victor Soares <victorsoares27306@gmail.com>
2025-10-31 11:59:16 -0300

    ATV. 05 - CONTROLLERS, VIEWS E ROTAS DOS CRUDS DE AUTHOR, CATEGORY, PUBLISHER E BOOK

---
+++ ATV. 05 - CONTROLLERS, VIEWS E ROTAS DOS CRUDS DE AUTHOR, CATEGORY, PUBLISHER E BOOK/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\BookController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::resource('books', BookController::class)->except(['create', 'store']);
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+
+Route::resource('categories', CategoryController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('publishers', PublisherController::class);
