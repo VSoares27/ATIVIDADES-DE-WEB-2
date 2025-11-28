670e21d19e54d8770d001d7907ea30df5f4c6db7
Victor Soares <victorsoares27306@gmail.com>
2025-11-28 10:11:15 -0300

    ATV. 07 - UPLOAD DE IMAGEM PARA CAPA DO LIVRO

--- ATV. 07 - UPLOAD DE IMAGEM PARA CAPA DO LIVRO/database/seeders/DatabaseSeeder.php
+++ ATV. 07 - UPLOAD DE IMAGEM PARA CAPA DO LIVRO/database/seeders/DatabaseSeeder.php
@@ -3,23 +3,30 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Author;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as FakerFactory;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        FakerFactory::create()->unique(true);
+        $this->call([
+        CategorySeeder::class,
+        AuthorPublisherBookSeeder::class,
+        UserBorrowingSeeder::class,
+        ]);
         // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
     }
 }
